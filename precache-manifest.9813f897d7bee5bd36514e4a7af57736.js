98838600ae510c70346fb5cad30b12c43709c2ac
Jongho Kim <april9288@gmail.com>
2019-04-04 23:13:44 -0700

    Updates

---
+++ precache-manifest.9813f897d7bee5bd36514e4a7af57736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dc696d38b28a1742538bb84b6787ddd",
+    "url": "/rightnowkbbq/static/media/bbq.4dc696d3.png"
+  },
+  {
+    "revision": "8111b434959da4c5fc7d",
+    "url": "/rightnowkbbq/static/js/runtime~main.425d1cc8.js"
+  },
+  {
+    "revision": "79b6ab3a09e280106149",
+    "url": "/rightnowkbbq/static/js/main.baa7e010.chunk.js"
+  },
+  {
+    "revision": "2099810ee9b5f299a7ad",
+    "url": "/rightnowkbbq/static/js/2.60ce64dc.chunk.js"
+  },
+  {
+    "revision": "79b6ab3a09e280106149",
+    "url": "/rightnowkbbq/static/css/main.40b643e6.chunk.css"
+  },
+  {
+    "revision": "d2d4552ed0c00dd6e47fcd3d0a3a3fb4",
+    "url": "/rightnowkbbq/index.html"
+  }
+];
